7117b20f4e96d6b8308e1b502b1b1ea58cfd2201
Jordan <jordunc@bgsu.edu>
2016-04-18 12:45:04 -0400

    Bug fix9

--- src/com.acumensolutions.bank/account/SavingsAccount.java
+++ src/com.acumensolutions.bank/account/SavingsAccount.java
@@ -14,7 +14,7 @@ public class SavingsAccount {
 	}
 	
 	public double applyInterest(int time) {
-		double amount = Math.pow(1.0 + (this.interestRate/((double)this.perYear)), (this.interestRate * (double)time));
+		double amount = Math.pow(1.0 + (this.interestRate/((double)this.perYear)), (this.perYear * (double)time));
 		amount = this.balance * amount;
 		return amount;
 	}
