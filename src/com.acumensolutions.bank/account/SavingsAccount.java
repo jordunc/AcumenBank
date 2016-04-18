5ea38426cd3eebbfb7ff9891b26f1233f6d14b79
Jordan <jordunc@bgsu.edu>
2016-04-18 11:42:18 -0400

    withdraw now private, compileInterest() method added

--- src/com.acumensolutions.bank/account/SavingsAccount.java
+++ src/com.acumensolutions.bank/account/SavingsAccount.java
@@ -2,4 +2,41 @@ package account;
 
 public class SavingsAccount {
 	private String ownerName;
+	private double balance;
+	
+	public CheckingAccount(String ownerName, double balance) {
+		this.ownerName = ownerName;
+		this.balance = balance;
+	}
+	
+	public double compileInterest(int time, double rate, int timesPerYear) {
+		return (this.balance * math.pow(1 + (rate/((double)*timesPerYear)), (rate * (double)time)));
+	}
+	
+	public void transfer(CheckingAccount destinationAccount, double amount) {
+		destinationAccount.deposit(amount);
+		this.withdraw(amount);
+	}
+	
+	public void deposit(double amount) {
+		if (amount < 0) {
+			throw new IllegalArgumentException("Cannot deposit a negative amount");
+		}
+		this.balance += amount;
+	}
+
+	private void withdraw(double amount) {
+		if (amount < 0) {
+			throw new IllegalArgumentException("Cannot withdraw a negative amount");
+		}
+		this.balance -= amount;
+	}
+
+	public String getOwnerName() {
+		return this.ownerName;
+	}
+
+	public double getBalance() {
+		return this.balance;
+	}
 }
